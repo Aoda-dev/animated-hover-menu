5533b8e8e01b64bb3b76af2566a19245f7042f59
Tolegen Kulseitov <hinokamiaoda@gmail.com>
2022-06-29 05:54:48 +0600

    init commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const links = document.querySelectorAll('a')
+const highLight = document.createElement('span')
+
+highLight.classList.add(
+	'bg-red-500',
+	'absolute',
+	'left-0',
+	'top-0',
+	'cursor-pointer',
+	'transition-all',
+	'duration-200',
+	'-z-10'
+)
+
+document.body.appendChild(highLight)
+
+links.forEach((link) => {
+	link.addEventListener('mouseover', (e) => {
+		const coords = e.target.getBoundingClientRect()
+		let top = coords.top + window.scrollY
+		let left = coords.left + window.scrollX
+
+		highLight.style.width = `${coords.width}px`
+		highLight.style.height = `${coords.height}px`
+		highLight.style.transform = `translate(${left}px, ${top}px)`
+	})
+})
